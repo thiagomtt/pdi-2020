82895f3b9004ccdffe021ac10861b2a2be3b1598
Thiago Moraes <tmoraes505@gmail.com>
2020-09-22 13:25:08 -0300

    100%

--- projeto1/espelhado.py
+++ projeto1/espelhado.py
@@ -25,7 +25,7 @@ def image_info(img):
 
 
 # Read original image 
-img = plt.imread('pacman.tiff')
+img = plt.imread('images/pacman.tiff')
 num_rows, num_cols = img.shape
 
 # Define o tamanho da borda em pixels
